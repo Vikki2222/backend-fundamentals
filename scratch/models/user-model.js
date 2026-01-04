022507331d6dd0e2debec5f44ba331f84801b7da
Vikki2222 <vigneshp18189@gmail.com>
2026-01-04 21:57:53 +0530

    finsihed the models where the Schema is made for the database and then connected the database

---
+++ scratch/models/user-model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+
+    fullname: {
+        type: String,
+        minLength : 3,
+        trim : true,
+    },
+    email: String,
+    password: String,
+
+    cart: {
+        type: Array,
+        default: [],
+    },
+
+    isadmin: Boolean,
+
+    orders: {
+        type: Array,
+        default: [],
+    },
+
+    contact: Number,
+    pictures: String,
+});
+
+module.exports = mongoose.model("user",userSchema);  //It creates a Mongoose model based on the schema and exports it so other files can perform database operations on the corresponding MongoDB collection//
