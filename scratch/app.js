7852af89e7a6f4dd56ebb3369567585d11dcdbee
Vikki2222 <vigneshp18189@gmail.com>
2026-01-06 16:38:35 +0530

    finished users api

--- scratch/app.js
+++ scratch/app.js
@@ -3,16 +3,21 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
+const ownersRouter = require ("./routes/ownersRouter");
+const productsRouter = require ("./routes/productsRouter");
+const usersRouter = require ("./routes/usersRouter");
+
 const db = require("./config/mongoose-connection");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.use(express.static(path.join(__dirname, "public")));
-
 app.set("view engine", "ejs");
 
+app.use("/users",usersRouter);
+app.use("/owners",ownersRouter);
+app.use("/products",productsRouter);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
